16406e9ee6f952b3c9c2ddaccdad332437aa1743
Gustavo <gustavo1046@hotmail.com>
2022-01-30 17:03:47 -0300

    finalizando tags de css em paginas 'update' do teacher

--- users/teacher/update/updatebackgroundboard.php
+++ users/teacher/update/updatebackgroundboard.php
@@ -13,6 +13,7 @@ $data = $consulta->fetch_assoc();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../styles/main.css">
     <title>Editar Imagem de Fundo</title>
 </head>
 
