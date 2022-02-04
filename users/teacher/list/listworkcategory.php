77c2f05c5443c22f444d358265c885494ac89d86
Marcus <marcusviniplay@hotmail.com>
2022-02-04 19:39:27 -0300

    Finalizei a Criação das tabelas do Usuário e do professor

--- users/teacher/list/listworkcategory.php
+++ users/teacher/list/listworkcategory.php
@@ -10,6 +10,17 @@
 </head>
 
 <body>
+    <h1 style="text-align: center;">Gerenciar Categoria Das Atividades</h1>
+    <nav>
+                <a href="../index.php">Home</a>
+                <a href="listwork.php">Gerenciar Atividades</a>
+                <a href="listworklevel.php">Gerenciar Nivel Das Atividades</a>
+                <a href="listworkcategory.php">Gerenciar Categoria Das Atividades</a>
+                <a href="listboard.php">Gerenciar Tabuleiros</a>
+                <a href="listbackgroundboard.php">Gerenciar Imagens de Fundo</a>
+                <a href="listimageboard.php">Gerenciar Imagens Tabuleiro</a>
+                <a href="../../../login/historicoacesso.php?opcao=Logout">Logout</a>
+    </nav>
     <main>
         <br><a href="../create/registerworkcategory.php">Cadastrar Nova Categoria</a>
         <?php
--- users/user/index.php
+++ users/user/index.php
@@ -6,15 +6,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../../styles/main.css">
+    <link rel="stylesheet" href="./style.css">
     <title>Atividades</title>
 </head>
 
 <body>
     <main>
-        <h1>Atividades Em Andamento</h1>
-        <a href="./initnewwork.php">Nova Atividade</a>
-        <a href="./finishedwork.php">Atividades Finalizadas</a>
-        <a href="../../login/historicoacesso.php?opcao=Logout">Logout</a>
+        <h1 style="text-align: center;">Atividades Em Andamento</h1>
+        <nav>
+            <a href="./index.php">Home</a>
+            <a href="./initnewwork.php">Nova Atividade</a>
+            <a href="./finishedwork.php">Atividades Finalizadas</a>
+            <a href="../../login/historicoacesso.php?opcao=Logout">Logout</a>
+        </nav>    
         <?php
         require_once '../../includes/classes/Conexao.php';
         require_once '../../includes/classes/Atividade_Aluno.php';
@@ -27,15 +31,31 @@
             $data_inicio = new DateTime($dados['Inicio']);
         ?>
             <hr>
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">Atividade: <?php echo $dados['Descricao']; ?></h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Status: <?php echo $dados['Status']; ?> </h6>
-                    <h6 class="card-subtitle mb-2 text-muted">Tabuleiro: <?php echo $dados['Tabuleiro']; ?> </h6>
-                    <p class="card-text">Iniciado em: <?php echo $data_inicio->format("d/m/Y H:i") ?></p>
-                    <a href="?id=<?php echo $dados['ID']; ?>" class="card-link">Continuar</a>
-                    <a href="./endwork.php?id=<?php echo $dados['ID']; ?>" class="card-link">Finalizar</a>
-                </div>
+            <div class="content">
+                <table class="TabelaU">    
+                    <thead> 
+                        <tr>
+                            <th>Atividade:</th>
+                            <td><?php echo $dados['Descricao']; ?></td>
+                        </tr>
+                        <tr>
+                            <th>Status:</th>
+                            <td><?php echo $dados['Status']; ?></td>
+                        </tr>
+                        <tr>
+                            <th>Tabuleiro:</th>
+                            <td> <?php echo $dados['Tabuleiro']; ?></td>
+                        </tr>                       
+                        <tr>
+                            <th>Iniciado em:</th>
+                            <td><?php echo $data_inicio->format("d/m/Y H:i") ?></td>
+                        </tr>
+                        <tr>
+                            <th><a href="?id=<?php echo $dados['ID']; ?>" class="card-link">Continuar</a></th>
+                            <td><a href="./endwork.php?id=<?php echo $dados['ID']; ?>" class="card-link">Finalizar</a></td>
+                     </tr>
+                    </thead>
+                </table>
             </div>
         <?php
         endwhile;
